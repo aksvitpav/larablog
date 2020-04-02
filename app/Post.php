bdc5918e4e59c902e0219153fd5418223ee51637
aksvitpav <aksvitpav@google.com>
2020-04-02 08:32:49 +0300

    add relation post-category-user, add post ctrl

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,14 @@ class Post extends Model
     protected $fillable = [
         'title', 'content',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function user() 
+    {
+        return $this->belongsTo('App\User');
+    }
 }
