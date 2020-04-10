b13958794236e5b73f7de7b2858cb17d6ea2daa1
aksvitpav <aksvitpav@gmail.com>
2020-04-11 00:25:28 +0300

    categories menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,16 +42,16 @@
 @section('navigation')
 <div class="card">
   <div class="card-body">
-<ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-        <a class="nav-link {{ (request()->is('/')) ? 'active' : '' }}" href="{{ route('welcome') }}">Все категории</a>
-    </li>
-    @foreach ($categories as $category)
-    <li class="nav-item">
-        <a class="nav-link {{ (request()->is('category/'.$category->id)) ? 'active' : '' }}" href="{{ route('welcome.category', $category->id) }}">{{ $category->name }} <span class="badge badge-secondary">{{$category->posts->count()}}</span></a>
-    </li>
-    @endforeach
-</ul>
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link {{ (request()->is('/')) ? 'active' : '' }}" href="{{ route('welcome') }}">Все категории</a>
+        </li>
+        @foreach ($categories as $category)
+        <li class="nav-item">
+            <a class="nav-link {{ (request()->is('category/'.$category->id)) ? 'active' : '' }}" href="{{ route('welcome.category', $category->id) }}">{{ $category->name }} <span class="badge badge-warning">{{$category->posts->count()}}</span></a>
+        </li>
+        @endforeach
+    </ul>
   </div>
 </div>
 @endsection
