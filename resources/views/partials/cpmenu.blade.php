4e925ddc10a8a11dc2d9bc4bbc61e5e8ea270d3f
aksvitpav <aksvitpav@gmail.com>
2020-04-11 00:35:30 +0300

    cpmenu blade

--- resources/views/partials/cpmenu.blade.php
+++ resources/views/partials/cpmenu.blade.php
@@ -1,14 +1,18 @@
-<ul class="nav nav-pills flex-column">
-  <li class="nav-item">
-    <a class="nav-link {{ (request()->is('home')) ? 'active' : '' }}" href="/home">Статистика</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link {{ (request()->is('home/posts')) ? 'active' : '' }}" href="{{ route('posts.index') }}">Посты</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link {{ (request()->is('home/categories')) ? 'active' : '' }}" href="{{ route('categories.index') }}">Категории</a>
-  </li>
-  <li class="nav-item">
-      <a class="nav-link {{ (request()->is('home/users')) ? 'active' : '' }}" href="{{ route('users.index') }}">Авторы</a>
-  </li>
-</ul>
+<div class="card">
+  <div class="card-body"> 
+    <ul class="nav nav-pills flex-column">
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('home')) ? 'active' : '' }}" href="/home">Статистика</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('home/posts')) ? 'active' : '' }}" href="{{ route('posts.index') }}">Посты</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('home/categories')) ? 'active' : '' }}" href="{{ route('categories.index') }}">Категории</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link {{ (request()->is('home/users')) ? 'active' : '' }}" href="{{ route('users.index') }}">Авторы</a>
+      </li>
+    </ul>
+  </div>
+</div>
